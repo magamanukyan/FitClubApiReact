15ec4c564edabb105f97ce9b8344a74245fbbd0f
magamanukyan <manukyanmaga98@gmail.com>
2023-05-19 19:55:36 +0400

    commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
     <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
       <img src={Logo} alt='logo' width='200px' height='40px'/>
       <Typography variant='h5' pb='40px' mt='20px'>
-        Made with ❤️ with Maga Manukyan
+        Made with ❤️ with Maga Manukyan.
       </Typography>
     </Stack>
    </Box>
